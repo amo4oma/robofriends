b3c8ce2daf2697bb862b63801b0c8482901b2469
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2021-02-14 00:59:18 +0800

    build

---
+++ build/precache-manifest.f55fd2e58e25c2e80596fcee7e0a11ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfacb9270e9e932372d8d52a0c9a650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01a37d429295cc509bb",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "2f7918049a9b3876248a",
+    "url": "/static/css/main.1bd60b8c.chunk.css"
+  },
+  {
+    "revision": "f01a37d429295cc509bb",
+    "url": "/static/js/2.abaed86f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.abaed86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7918049a9b3876248a",
+    "url": "/static/js/main.e7ce1c8e.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
